53a48e16bcaed89a3419db69d9aa8cf2115d16ab
soma <ogbennaanita@gmail.com>
2024-06-07 03:51:18 +0100

    updates

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,63 +1,26 @@
-const { expect } = require('chai');
 const calculateNumber = require('./2-calcul_chai');
+const { expect } = require('chai');
 
 describe('calculateNumber', () => {
-    describe('SUM', () => {
-        it('should return 4 when inputs are 1 and 3', () => {
-            expect(calculateNumber('SUM', 1, 3)).to.equal(4);
-        });
+  describe('when type is SUM', () => {
+    it('should return the sum of two rounded numbers', () => {
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    });
+  });
 
-        it('should return 5 when inputs are 1.2 and 3.7', () => {
-            expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
-        });
+  describe('when type is SUBTRACT', () => {
+    it('should return the subtraction of two rounded numbers', () => {
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    });
+  });
 
-        it('should return 5 when inputs are 1.5 and 3.2', () => {
-            expect(calculateNumber('SUM', 1.5, 3.2)).to.equal(5);
-        });
-
-        it('should return 6 when inputs are 1.5 and 4.5', () => {
-            expect(calculateNumber('SUM', 1.5, 4.5)).to.equal(6);
-        });
-
-        it('should return 0 when inputs are 0.4 and 0.4', () => {
-            expect(calculateNumber('SUM', 0.4, 0.4)).to.equal(0);
-        });
+  describe('when type is DIVIDE', () => {
+    it('should return the division of two rounded numbers', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
     });
 
-    describe('SUBTRACT', () => {
-        it('should return -2 when inputs are 1 and 3', () => {
-            expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
-        });
-
-        it('should return -3 when inputs are 1.2 and 4.7', () => {
-            expect(calculateNumber('SUBTRACT', 1.2, 4.7)).to.equal(-3);
-        });
-
-        it('should return 0 when inputs are 3.5 and 3.5', () => {
-            expect(calculateNumber('SUBTRACT', 3.5, 3.5)).to.equal(0);
-        });
+    it('should return "Error" when trying to divide by zero', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
     });
-
-    describe('DIVIDE', () => {
-        it('should return 0.25 when inputs are 1 and 4', () => {
-            expect(calculateNumber('DIVIDE', 1, 4)).to.equal(0.25);
-        });
-
-        it('should return 1 when inputs are 4.5 and 4.5', () => {
-            expect(calculateNumber('DIVIDE', 4.5, 4.5)).to.equal(1);
-        });
-
-        it('should return "Error" when inputs are 1 and 0.4', () => {
-            expect(calculateNumber('DIVIDE', 1, 0.4)).to.equal('Error');
-        });
-
-        it('should return "Error" when inputs are 1 and 0', () => {
-            expect(calculateNumber('DIVIDE', 1, 0)).to.equal('Error');
-        });
-    });
-
-    it('should throw an error for invalid type', () => {
-        expect(() => calculateNumber('MULTIPLY', 1, 3)).to.throw('Invalid type');
-    });
+  });
 });
-
