755b8604332f49ccc8684ca5928353d524fed181
soma <ogbennaanita@gmail.com>
2024-06-07 03:46:59 +0100

    updates

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,25 +1,20 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', () => {
-    it('should return 4 when inputs are 1 and 3', () => {
-        assert.strictEqual(calculateNumber(1, 3), 4);
+describe('calculateNumber', function() {
+    it('Rounding two integer numbers',function() {
+        assert.equal(calculateNumber(1, 3), 4);
     });
-
-    it('should return 5 when inputs are 1.2 and 3.7', () => {
-        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    it('Rounding one integer and the other float', function() {
+	assert.equal(calculateNumber(1, 3.7), 5);
     });
-
-    it('should return 5 when inputs are 1.5 and 3.2', () => {
-        assert.strictEqual(calculateNumber(1.5, 3.2), 5);
+    it('Rounding float numbers with one roundup', function() {
+        assert.equal(calculateNumber(1.2, 3.7), 5);
     });
-
-    it('should return 6 when inputs are 1.5 and 4.5', () => {
-        assert.strictEqual(calculateNumber(1.5, 4.5), 6);
+    it('Rounding float numbers with both roundup', function() {
+        assert.equal(calculateNumber(1.5, 3.7), 6);
     });
-
-    it('should return 0 when inputs are 0.4 and 0.4', () => {
-        assert.strictEqual(calculateNumber(0.4, 0.4), 0);
+    it('Rounding float trailing numbers with both rounddown', function() {
+        assert.equal(calculateNumber(1.499999, 3.499999), 4);
     });
 });
-
